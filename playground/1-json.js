c2e05ef5e83f68b75fc3bb43a39ba696de866f2f
Luke G <lukegronert@gmail.com>
2020-11-03 13:25:46 +0800

    Added JSON practice to playground

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,4 +1,20 @@
-const book = {
-    title: '',
-    author: ''
-}
+const fs = require('fs');
+// const book = {
+//     title: 'Name of the Wind',
+//     author: 'Patrick Rothfuss'
+// }
+
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-JSON.json', bookJSON);
+
+// const dataBuffer = fs.readFileSync('1-JSON.json');
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.title)
+
+const dataBuffer = fs.readFileSync('1-JSON.json')
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+data.name = "Luke";
+data.age = 28;
+fs.writeFileSync('1-json.json', JSON.stringify(data));
