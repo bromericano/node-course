9ee4837e4440e9d70d404a6e8881e1e13a89b7de
Luke G <lukegronert@gmail.com>
2020-11-14 17:08:06 +0800

    Practices API calls for weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,17 @@
-console.log('Starting...')
+const request = require('request');
 
-setTimeout(() => {
-    console.log('2 second timer')
-}, 2000)
+// const url = 'http://api.weatherstack.com/current?access_key=b0572a2fb0165bd4e7ac2a9604a7a215&query=37.8267,-122.4233&units=f'
 
-setTimeout(() => {
-    console.log('0 second timer')
-}, 0)
+// request({ url: url, json: true }, (error, response) => {
+//     console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees.')
+// })
 
-console.log('Stopping...')
+// Geocoding
+// Address -> Lat/Long -> Weather
+
+const geocodeurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYnJvbWVyaWNhbm8iLCJhIjoiY2toaGZvdWtqMGl2eDJybnFhYm9qZWlpMSJ9.19GpgPl9eynN2-z8nNrkdw&limit=1'
+request({ url: geocodeurl, json: true }, (error, response) => {
+    const lat = response.body.features[0].center[1];
+    const long = response.body.features[0].center[0];
+    console.log(lat, long)
+})
