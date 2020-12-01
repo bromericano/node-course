4c4797ffde9b9e668b6aefc54db486493f31bdad
Luke G <lukegronert@gmail.com>
2020-12-01 12:39:48 +0800

    Set up partials and styled pages through styles.css

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,20 @@
 const path = require('path')
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 //Set handlebar engine
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//set up static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 //Set up static directory
@@ -37,6 +44,22 @@ app.get('/weather', (req, res) => {
     res.send({
         forecast: '52F',
         location: 'Taipei'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404page', {
+        title: '404',
+        name: 'Luke G',
+        message: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404page', {
+        title: '404',
+        name: 'Luke G',
+        message: 'Page not found'
     })
 })
 
